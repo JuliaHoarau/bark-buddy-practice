83a4afa92b557968d17ad92f7461eac485a8ce8e
Julia Hoarau <j.hoarau97@outlook.com>
2024-11-19 17:08:30 +1000

    Created dog profile routes on the "pet profiles" page to link to the respective dog profile/:id pages

--- pages/dogProfiles/index.vue
+++ pages/dogProfiles/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 // Import necessary Vue utilities
 import { ref, onMounted } from 'vue';
+import { ChevronRightIcon } from '@heroicons/vue/24/solid';
 
 // Create a reactive variable to hold the list of dog profiles
 const dogProfiles = ref([]);
@@ -22,35 +23,45 @@ async function fetchDogProfiles() {
 onMounted(() => {
   fetchDogProfiles();
 });
-
-// Dynamic IDs for dog profile card sections
-const dogCardId = (id: string) => `dog-card-${id}`;
-const dogPhotoId = (id: string) => `dog-photo-${id}`;
-const dogDetailsId = (id: string) => `dog-details-${id}`;
-const dogExtrasId = (id: string) => `dog-extras-${id}`;
-
 </script>
 
 <template>
   <section class="p-5 max-w-screen">
     <h1 class="text-xl">Pet Profiles</h1>
 
-      <!-- Loop through and display each dog's name with a link to their profile -->
-      <div v-for="dog in dogProfiles" :key="dog.dogProfileID" class="container flex flex-row mt-10 " >
+    <!-- Loop through and display each dog's name with a link to their profile -->
+    <div
+      v-for="dog in dogProfiles"
+      :key="dog.dogProfileID"
+      class="container flex flex-row mt-10 rounded-lg"
+    >
+      <router-link
+        :to="{ name: 'dogProfiles-id', params: { id: dog.dogProfileID } }"
+        class="flex flex-row w-full items-center"
+      >
         <div>
-        <img :src="dog.profileImage" alt="Photo of {{ dog.dogName }}" class="rounded-full w-[60px] aspect-square object-cover" />
-      </div >
-      <div class="flex flex-col ml-5 w-full">
-        <h2 class="text-base font-semibold mb-2">{{ dog.dogName }}</h2>
-        <div class="flex flex-row space-x-4 text-xs w-auto">
-        <p>{{  dog.breed }}</p>
-        <!-- TODO: add if statement for months -->
-        <p>{{ dog.age }} years</p> 
-        <p>{{ dog.weight }}kg</p>
-        <p> {{dog.temperament}}</p>
-      </div>
-      </div>
-      </div>
-
-    </section>
+          <img
+            :src="dog.profileImage"
+            alt="Photo of {{ dog.dogName }}"
+            class="rounded-full w-[60px] aspect-square object-cover"
+          />
+        </div>
+        <div class="flex flex-col ml-5 w-full">
+          <h2 class="text-base font-semibold mb-2">{{ dog.dogName }}</h2>
+          <div class="flex flex-row  text-sm w-auto text-zinc-700 items-baseline">
+            <div class="flex flex-row space-x-3">
+            <p>{{ dog.breed }}</p>
+            <!-- TODO: add if statement for months -->
+            <p>{{ dog.age }} years</p>
+            <p>{{ dog.weight }}kg</p>
+            <p>{{ dog.temperament }}</p>
+          </div>
+          <div class=" ml-auto">
+            <ChevronRightIcon class="h-3 w-6" />
+          </div>
+          </div>
+        </div>
+      </router-link>
+    </div>
+  </section>
 </template>
